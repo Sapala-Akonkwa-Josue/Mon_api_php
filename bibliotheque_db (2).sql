1e5dc5b379177eae0f47a312f305921eca2a4c89
Sapala-Akonkwa-Josue <sapalajosue8@gmail.com>
2025-03-10 13:28:02 +0100

    api4

--- bibliotheque_db (2).sql
+++ bibliotheque_db (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 05 mars 2025 à 13:45
+-- Généré le : lun. 10 mars 2025 à 13:24
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -35,6 +35,13 @@ CREATE TABLE `auteur` (
   `image` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `auteur`
+--
+
+INSERT INTO `auteur` (`id_auteur`, `nom_auteur`, `prenom_auteur`, `id_nationalite`, `image`) VALUES
+(6, 'Jean-pierre', 'de ROUSSEAU', 7, 'images/auteurs/hero.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +52,15 @@ CREATE TABLE `categorie` (
   `id_categorie` int(11) NOT NULL,
   `nom_categorie` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id_categorie`, `nom_categorie`) VALUES
+(5, 'Theatre'),
+(6, 'Blague'),
+(7, 'Poeme');
 
 -- --------------------------------------------------------
 
@@ -58,6 +74,15 @@ CREATE TABLE `nationalite` (
   `image` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `nationalite`
+--
+
+INSERT INTO `nationalite` (`id_nationalite`, `nom_nationalite`, `image`) VALUES
+(6, 'Anglaise', 'images/nationalites/profil0.jpg'),
+(7, 'Libanais', 'images/nationalites/profil3.jpg'),
+(8, 'Chinois', 'images/nationalites/profil2.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -69,9 +94,17 @@ CREATE TABLE `ouvrage` (
   `titre_ouvrage` varchar(255) NOT NULL,
   `id_auteur` int(11) DEFAULT NULL,
   `id_categorie` int(11) DEFAULT NULL,
-  `annee_publication` year(4) DEFAULT NULL,
+  `annee_publication` year(4) DEFAULT year(curdate()),
   `image` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `ouvrage`
+--
+
+INSERT INTO `ouvrage` (`id_ouvrage`, `titre_ouvrage`, `id_auteur`, `id_categorie`, `annee_publication`, `image`) VALUES
+(4, 'J\'ai epouse une religieuse', 6, 7, '1999', 'images/ouvrages/her.jpg'),
+(5, 'J\'ai epouse une religieuse', 6, 7, NULL, 'images/ouvrages/her.jpg');
 
 -- --------------------------------------------------------
 
@@ -90,6 +123,15 @@ CREATE TABLE `users` (
   `date_inscription` timestamp NOT NULL DEFAULT current_timestamp(),
   `id_nationalite` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `prenom`, `email`, `password`, `bio`, `image`, `date_inscription`, `id_nationalite`) VALUES
+(5, 'Sapala', 'josue', 'sapala@gmail.com', '$2y$10$HtEpfGss8mzzlcVwXoWpEuNj4127V3ev9yPScoB88og2EQF.3FRwu', 'Une histoire nourrit ma sante mais une contes me forme mentalement.', 'images/users/client1.jpg', '2025-03-07 12:39:46', 8),
+(8, 'Iragi', 'Hervey', 'iragi@gmail.com', '$2y$10$8KrfViJyIpgroKBrkbciV./P.SvyvkJfCUMirx4P.6Id2jgdFFvEa', 'L\'afrique ecrira sa propre histoire, je suis conscient.', 'images/users/team2.jpg', '2025-03-07 13:09:11', 7),
+(9, 'thierry', 'nirere', 'nirer@gmail.com', '$2y$10$/Crz7MzlQ2toRGtknltii.Ib1GthdJ6FlkUIsu89h.sXuwVxByP9K', 'l\'afrique ecrira sa propre histoire', 'images/users/team3.jpg', '2025-03-08 11:12:07', 7);
 
 --
 -- Index pour les tables déchargées
@@ -138,31 +180,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `auteur`
 --
 ALTER TABLE `auteur`
-  MODIFY `id_auteur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_auteur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `nationalite`
 --
 ALTER TABLE `nationalite`
-  MODIFY `id_nationalite` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_nationalite` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `ouvrage`
 --
 ALTER TABLE `ouvrage`
-  MODIFY `id_ouvrage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_ouvrage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Contraintes pour les tables déchargées
